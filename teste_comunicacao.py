cb5c78139235a0c48b30decc03d34de149ad4d51
Gustavo Guedes <gustavoipguedes@gmail.com>
2021-11-09 20:07:59 -0300

    teste: Comunicacao tcpip

---
+++ teste_comunicacao.py
@@ -0,0 +1,18 @@
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+server_address = ('localhost', 10000)
+
+sock.bind(server_address)
+
+
+# Listen for incoming connections
+sock.listen(1)
+
+while True:
+    # Wait for a connection
+    connection, client_address = sock.accept()
