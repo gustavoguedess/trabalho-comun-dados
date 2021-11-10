ddaa2b4b6b6edc925f6fc79f2cdd071a93b0badb
Gustavo Guedes <gustavoipguedes@gmail.com>
2021-11-10 08:43:58 -0300

    test: Testes de protocolo TCP IP funcionando

--- teste_comunicacao.py
+++ teste_comunicacao.py
@@ -16,3 +16,15 @@ sock.listen(1)
 while True:
     # Wait for a connection
     connection, client_address = sock.accept()
+
+#%%
+import socket
+
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.connect(('www.python.org', 80))
+
+def servidor():
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((socket.gethostname(), 1234))
+    serversocket.listen(5)
+servidor()
