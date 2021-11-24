232c2805bd36f37e66f4f2af40e7854f1dbbaee7
Gustavo Guedes <gustavoipguedes@gmail.com>
2021-11-24 08:25:25 -0300

    feat: CORS adicionado ao server API

--- server_api.py
+++ server_api.py
@@ -3,7 +3,18 @@ import uvicorn
 
 from pydantic import BaseModel
 
+from fastapi.middleware.cors import CORSMiddleware 
+
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 messages_list = []
 
