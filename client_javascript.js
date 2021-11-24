f7b9e9a10aa177d99c0049cbb2fe663566ab6815
Gustavo Guedes <gustavoipguedes@gmail.com>
2021-11-24 09:08:13 -0300

    fix: Encode e send funcionando

--- client_javascript.js
+++ client_javascript.js
@@ -39,9 +39,9 @@ function encode ( message ){
           message: message
         }
     };
-    
+    console.log(message);
     return api.request(options)
-        .then(res => res)
+        .then(res => res.data)
         .catch(err => console.log("Erro Encode"));
 }
 
@@ -53,40 +53,29 @@ function send_message (origin_username, dest_username, message){
         url: URL,
         headers: {'Content-Type': 'application/json'},
         data: {
-            message: response.data.message
+            message: message
         }
     };
     
     return api.request(options)
-        .then(res => res)
+        .then(res => res.data)
         .catch(err => console.log("Erro Send Message"));
 }
 
 
-//var data = send_message("guedes", "vanin", "testando");
 async function teste(){
     var res_listen = await listen("a")
-        .then(res => res)
-        .catch(err => console.log("Erro Retorno Listen"));
     console.log(res_listen);
     
     var res_decode = await decode(res_listen.message)
-        .then(res => res)
-        .catch(err => console.log("Erro Retorno Decode"));
     console.log(res_decode);
 };
 
 async function teste2(){
     var res_encode = await encode("testando")
-        .then(res =>res)
-        .catch(err => console.log("Erro Retorno Encode"));
     console.log(res_encode);
 
-    /*var res_send = await send_message("guedes", "a", res_encode.encoded)
-        .then(res => res)
-        .catch(err => console.log("Erro restorno Send Message"));
-    console.log(res_send);*/
+    var res_send = await send_message("guedes", "a", res_encode.encoded)
+    console.log(res_send);
 }
 
-
-teste();
