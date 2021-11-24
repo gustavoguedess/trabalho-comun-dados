107b9e86a31dfe671c36df95fe747bb47f740eee
Gustavo Guedes <gustavoipguedes@gmail.com>
2021-11-24 08:58:28 -0300

    fix: Listen e decode alterados

--- client_javascript.js
+++ client_javascript.js
@@ -15,29 +15,21 @@ function decode ( message ) {
         }
       };
       
-      api.request(options).then(function (response) {
-        console.log(response.data);
-      }).catch(function (error) {
-        console.error(error);
-      });
+    return api.request(options)
+        .then(res => res.data)
+        .catch(err => console.log("Erro Decode"));
 }
+
 function listen (username) {
     URL = "listen/"+username
     console.log(URL);
 
-    api.get(URL)
-        .then((res) => {
-            console.log(res.data);
-            decode(res.data.message);
-            return res.data;
-        })
-        .catch(function (error) {
-            console.log("error");
-            return null;
-        });
+    return api.get(URL)
+        .then(response => response.data)
+        .catch(err => console.log("Erro listen"));
 };
 
-function send_message (origin_username, dest_username, message){
+function encode ( message ){
     URL = "encode/"
     const options = {
         method: 'GET',
@@ -48,35 +40,53 @@ function send_message (origin_username, dest_username, message){
         }
     };
     
-    api.request(options).then(function (response) {
-        URL = "send/"+origin_username+"/"+dest_username;
-        message = encode(message);
-        console.log(message);
+    return api.request(options)
+        .then(res => res)
+        .catch(err => console.log("Erro Encode"));
+}
 
-        const options = {
-            method: 'POST',
-            url: URL,
-            headers: {'Content-Type': 'application/json'},
-            data: {
-                message: response.data.message
-            }
-        };
-        console.log(options);
-        api.request(options).then(function (response) {
-            console.log(response.data);
-        }).catch(function (error) {
-            console.error("error");
-        });
+function send_message (origin_username, dest_username, message){
+    URL = "send/"+origin_username+"/"+dest_username;
 
-        return response.data;
-    }).catch(function (error) {
-        console.error(error);
-    });
-
+    const options = {
+        method: 'POST',
+        url: URL,
+        headers: {'Content-Type': 'application/json'},
+        data: {
+            message: response.data.message
+        }
+    };
     
+    return api.request(options)
+        .then(res => res)
+        .catch(err => console.log("Erro Send Message"));
 }
 
 
-var data = send_message("guedes", "vanin", "testando");
+//var data = send_message("guedes", "vanin", "testando");
+async function teste(){
+    var res_listen = await listen("a")
+        .then(res => res)
+        .catch(err => console.log("Erro Retorno Listen"));
+    console.log(res_listen);
+    
+    var res_decode = await decode(res_listen.message)
+        .then(res => res)
+        .catch(err => console.log("Erro Retorno Decode"));
+    console.log(res_decode);
+};
+
+async function teste2(){
+    var res_encode = await encode("testando")
+        .then(res =>res)
+        .catch(err => console.log("Erro Retorno Encode"));
+    console.log(res_encode);
+
+    /*var res_send = await send_message("guedes", "a", res_encode.encoded)
+        .then(res => res)
+        .catch(err => console.log("Erro restorno Send Message"));
+    console.log(res_send);*/
+}
 
 
+teste();
